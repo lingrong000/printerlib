550ea8a709c39d60eca2bdee8bbe8a43594bb1e4
lingrong000 <lingrong000@gmail.com>
2026-03-25 10:15:59 +0800

    去除日期版本号

--- printer/build.gradle.kts
+++ printer/build.gradle.kts
@@ -1,45 +1,8 @@
-import java.time.LocalDate
-import java.util.Properties
-
 plugins {
     alias(libs.plugins.android.library)
     kotlin("android")
     id("maven-publish") // Maven发布插件
 }
-
-// 生成版本号：yyyyMMddxx，支持自动递增
-fun generateVersionName(): String {
-    val date = LocalDate.now()
-    val dateStr = String.format("%04d%02d%02d", date.year, date.monthValue, date.dayOfMonth)
-    
-    // 版本文件路径
-    val versionFile = file("version.properties")
-    
-    // 读取现有版本
-    var sequence = 1
-    if (versionFile.exists()) {
-        val props = Properties()
-        versionFile.inputStream().use { props.load(it) }
-        val savedDate = props.getProperty("date", "")
-        val savedSequence = props.getProperty("sequence", "0").toIntOrNull() ?: 0
-        
-        if (savedDate == dateStr) {
-            // 同一天，递增序号
-            sequence = savedSequence + 1
-        }
-        // 不同天，序号重置为1
-    }
-    
-    // 保存新版本
-    val props = Properties()
-    props.setProperty("date", dateStr)
-    props.setProperty("sequence", sequence.toString())
-    versionFile.outputStream().use { props.store(it, "Version info") }
-    
-    return "${dateStr}${String.format("%02d", sequence)}"
-}
-
-val versionName = generateVersionName()
 
 android {
     namespace = "com.cumtenn.printer"
@@ -70,14 +33,6 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    
-    // 配置AAR输出文件名
-    libraryVariants.all {
-        outputs.all {
-            val output = this as com.android.build.gradle.internal.api.LibraryVariantOutputImpl
-            output.outputFileName = "printer_${versionName}.aar"
-        }
-    }
 }
 
 dependencies {
@@ -101,7 +56,7 @@ publishing {
                 from(components["release"])
                 groupId = "com.github.lingrong000"
                 artifactId = "printer"
-                version = versionName
+                version = "v1.1.4"
             }
         }
     }
